45dda08e4ec127d3769a7d988a548b68bdbe47fb
Andres Suarez Guzman <andres_s18@outlook.com>
2025-05-29 16:33:57 -0500

    connection updated to support cosmosDB

--- Readme.md
+++ Readme.md
@@ -3,7 +3,7 @@ Common libraries used by Play Economy services
 
 ## Create and publish package
 ```powershell
-$version="1.0.6"
+$version="1.0.7"
 $owner="dotnetMicroservicesCourseASGX"
 $gh_pat="[PAT HERE]"
 
--- src/Play.Common/Settings/MongoDbSettings.cs
+++ src/Play.Common/Settings/MongoDbSettings.cs
@@ -6,6 +6,12 @@ namespace Play.Common.Settings
 
         public int Port { get; init; }
 
-        public string ConnectionString => $"mongodb://{Host}:{Port}";
+        private string connectionString;
+        public string ConnectionString
+        {
+            get { return string.IsNullOrWhiteSpace(connectionString) ? $"mongodb://{Host}:{Port}" : connectionString; }
+            init { connectionString = value; }
+        }
+
     }
 }
